2a62181020941c3237bb002acc76967e25199339
skofkyo <kyo7350201@gmail.com>
2015-12-21 22:38:38 +0800

    Update RemoveoVerflowable.uc.js

--- ToolbarScript/RemoveoVerflowable.uc.js
+++ ToolbarScript/RemoveoVerflowable.uc.js
@@ -1,6 +1,13 @@
+// ==UserScript==
+// @name           RemoveOverflowable.uc.js
+// @description    移除按鈕收合的功能
+// @author         skofkyo
+// @include        main
+// @license        MIT License
+// @compatibility  Firefox 41+
+// @charset        UTF-8
+// @version        0.1
+// @homepageURL    no
+// ==/UserScript==
+
 document.getElementById("nav-bar").removeAttribute("overflowable");
-
-const preferences = require('sdk/simple-prefs');
-const NewTabURL = require('resource:///modules/NewTabURL.jsm').NewTabURL;
-
-NewTabURL.override('about:addons');
